33743d50ebf217e473691a432d318b0417f600cc
Prince Oncada <prince.oncada@gmail.com>
2023-10-13 07:54:43 +0800

    initial commit test JWT

---
+++ jwt-implementation-spring-react/client/src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/App.css'
+import Routing from "./Routing.jsx";
+
+function App() {
+  return (
+      <div className="body">
+          <Routing/>
+      </div>
+  )
+}
+
+export default App

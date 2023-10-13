27d6afddda81ab7a050b4399f08732c3979e1dd8
Prince Oncada <prince.oncada@gmail.com>
2023-10-13 20:27:18 +0800

    Authentication Success Handler: replaced the authentication endpoint with this for a more control and better organization of tasks

---
+++ jwt-implementation-spring-react/server/src/main/kotlin/com/winners/server/config/GoogleOAuthAuthenticationSuccessHandler.kt
@@ -0,0 +1,43 @@
+package com.winners.server.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.winners.server.config.service.CustomUserDetailsService
+import com.winners.server.config.service.JwtService
+import jakarta.servlet.http.Cookie
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.security.core.Authentication
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler
+
+class GoogleOAuthAuthenticationSuccessHandler(
+    private val authenticationManager: AuthenticationManager,
+    private val customUserDetailsService: CustomUserDetailsService,
+    private val jwtService: JwtService,
+): AuthenticationSuccessHandler {
+    override fun onAuthenticationSuccess(
+        request: HttpServletRequest?,
+        response: HttpServletResponse?,
+        authentication: Authentication?,
+    ) {
+        try {
+            authenticationManager.authenticate(authentication)
+        } catch (e: Exception) {
+            throw BadCredentialsException("User not found with email: ${authentication?.name}")
+        }
+
+        val userDetails = customUserDetailsService.loadUserByUsername(authentication?.name!!)
+        val token = jwtService.generateToken(userDetails)
+
+        if (response != null) {
+            val cookie = Cookie("jwtToken", token).apply {
+                maxAge = 3600
+                path = "/"
+                domain = "localhost"
+            }
+            response.addCookie(cookie)
+            response.sendRedirect("http://localhost:5000/authenticate")
+        }
+    }
+}

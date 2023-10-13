c51fbe9cd7a17688e20408f901880dbf8b4dcdb0
Prince Oncada <prince.oncada@gmail.com>
2023-10-13 20:33:15 +0800

    Refactor: changed several files name to start with Google --defined the googleOAuthAuthenticationManager --defined the googleOAuthAuthenticationSuccessHandler --defined the corsFilter --refactored the securityFilterChain to enable CORS, to use the GoogleOAuthAuthenticationSuccessHandler for oauth2Login, set session management creation policy to stateless, and included the JwtAuthenticationFilter as well.

--- jwt-implementation-spring-react/server/src/main/kotlin/com/winners/server/config/SecurityConfig.kt
+++ jwt-implementation-spring-react/server/src/main/kotlin/com/winners/server/config/SecurityConfig.kt
@@ -1,54 +1,83 @@
 package com.winners.server.config
 
-import com.winners.server.config.service.CustomOidcUserService
-import com.winners.server.config.service.CustomUserDetailsService
-import com.winners.server.config.service.JwtService
+import com.winners.server.config.service.*
+import com.winners.server.domain.repository.UserRepository
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.authentication.ProviderManager
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
 @Configuration
 @EnableWebSecurity
 class SecurityConfig(
     private val jwtService: JwtService,
-    private val customUserDetailsService: CustomUserDetailsService
+    private val customUserDetailsService: CustomUserDetailsService,
+    private val googleOAuthAuthenticationProvider: GoogleOAuthAuthenticationProvider
 ) {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http {
             csrf { disable() }
-            //TODO("Enable CORS")
+            cors {  }
             authorizeHttpRequests {
-                authorize(anyRequest, permitAll)
+                authorize(anyRequest, authenticated)
             }
             oauth2Login {
                 userInfoEndpoint {
                     oidcUserService = oidcUserService()
                 }
-                //TODO("defaultSuccessUrl")
+                authenticationSuccessHandler = googleOAuthAuthenticationSuccessHandler()
             }
-            //TODO("Add JWT authentication filter")
+            sessionManagement {
+                sessionCreationPolicy = SessionCreationPolicy.STATELESS
+            }
+            addFilterBefore<UsernamePasswordAuthenticationFilter>(
+                JwtAuthenticationFilter(
+                    jwtService,
+                    customUserDetailsService
+                )
+            )
         }
-
         return http.build()
     }
 
-    //TODO("Add CORS filter")
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration().applyPermitDefaultValues()
+        config.allowedOrigins = listOf("*")
+        config.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        config.allowedHeaders = listOf("content-type", "authorization", "X-Access-Token")
+        source.registerCorsConfiguration("/**", config)
+        return CorsFilter(source)
+    }
 
     @Bean
-    fun passwordEncoder(): PasswordEncoder {
-        return BCryptPasswordEncoder()
+    fun googleOAuthAuthenticationSuccessHandler(): GoogleOAuthAuthenticationSuccessHandler {
+        return GoogleOAuthAuthenticationSuccessHandler(
+            googleOAuthAuthenticationManager(googleOAuthAuthenticationProvider),
+            customUserDetailsService,
+            jwtService
+        )
+    }
+
+    @Bean
+    fun googleOAuthAuthenticationManager(googleOAuthAuthenticationProvider: GoogleOAuthAuthenticationProvider): AuthenticationManager {
+        return ProviderManager(listOf(googleOAuthAuthenticationProvider))
     }
 
     @Bean
     fun oidcUserService(): OidcUserService {
-        return CustomOidcUserService(OidcUserService())
+        return GoogleOidcUserService(OidcUserService())
     }
 }

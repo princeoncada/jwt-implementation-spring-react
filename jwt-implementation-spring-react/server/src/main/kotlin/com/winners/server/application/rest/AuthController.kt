8dce2ab0d1473feb1f2502740a56d5129c6c063b
Prince Oncada <prince.oncada@gmail.com>
2023-10-13 20:24:50 +0800

    Refactor: removed authentication end point and replaced it with GoogleOAuthAuthenticationSuccessHandler to process and generate token for the client side

--- jwt-implementation-spring-react/server/src/main/kotlin/com/winners/server/application/rest/AuthController.kt
+++ jwt-implementation-spring-react/server/src/main/kotlin/com/winners/server/application/rest/AuthController.kt
@@ -3,6 +3,7 @@ package com.winners.server.application.rest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -11,15 +12,9 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/auth")
-class AuthController{
+class AuthController {
     @GetMapping("/hello")
     fun hello() {
         println("Hello World!")
-    }
-
-    @PostMapping("/authenticate")
-    fun createAuthenticationToken(): ResponseEntity<String> {
-        AuthenticationManager().authenticate()
-        println("Login")
     }
 }

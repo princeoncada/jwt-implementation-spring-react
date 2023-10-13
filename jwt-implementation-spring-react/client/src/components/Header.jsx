8498128d337828be7d0f01867aa4b3c745bf2bf0
Prince Oncada <prince.oncada@gmail.com>
2023-10-13 20:34:39 +0800

    Component: included header and footer for authenticated pages

---
+++ jwt-implementation-spring-react/client/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+    return (
+        <header>
+            <nav className="nav">
+                <div className="nav__links">
+                    <Link to="/">
+                        <div className="nav__link">Home</div>
+                    </Link>
+                    <div>|</div>
+                    <Link to="/about">
+                        <div className="nav__link">Stock Screen</div>
+                    </Link>
+                    <div>|</div>
+                    <Link to="/contact">
+                        <div className="nav__link">Stock Analysis</div>
+                    </Link>
+                    <div>|</div>
+                    <Link to="/logout">
+                        <div className="nav__link">Logout</div>
+                    </Link>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;

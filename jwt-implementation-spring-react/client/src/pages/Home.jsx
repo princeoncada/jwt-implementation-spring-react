4f44e41912d78c0e2520a0b6113a47f4b2634b1e
Prince Oncada <prince.oncada@gmail.com>
2023-10-13 20:34:52 +0800

    Page: default home page

---
+++ jwt-implementation-spring-react/client/src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import Header from "../components/Header.jsx";
+import Footer from "../components/Footer.jsx";
+import Cookies from "js-cookie";
+import {useEffect, useState} from "react";
+
+function Home() {
+    const [token, setToken] = useState(null);
+
+    useEffect(() => {
+        setToken(Cookies.get("jwtToken"));
+    }, []);
+
+    function logToken() {
+        console.log(token);
+    }
+
+    return (
+        <>
+            <Header/>
+            <main>
+                <h1>Home</h1>
+                <p>This is the home page</p>
+                <button onClick={logToken}>View Token</button>
+            </main>
+            <Footer/>
+        </>
+    )
+}
+
+export default Home;

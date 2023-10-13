d1c81d4736e03e2e0a95044980cb649f8d1f5387
Prince Oncada <prince.oncada@gmail.com>
2023-10-13 20:37:30 +0800

    Refactor: added routing for the new components and pages --also included an authenticated state

--- jwt-implementation-spring-react/client/src/Routing.jsx
+++ jwt-implementation-spring-react/client/src/Routing.jsx
@@ -1,11 +1,31 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "./pages/Login.jsx";
+import {useState} from "react";
+import Protected from "./Protected.jsx";
+import Home from "./pages/Home.jsx";
+import Authenticate from "./redirect/Authenticate.jsx";
+import Logout from "./redirect/Logout.jsx";
 
 function Routing() {
+    const [isAuthenticated, setAuthenticated] = useState(false);
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Login />} />
+
+                <Route path="/home" element={
+                    <Protected isAuthenticated={isAuthenticated}>
+                        <Home/>
+                    </Protected>
+                } />
+
+                <Route path="/logout" element={
+                    <Protected isAuthenticated={isAuthenticated}>
+                        <Logout setAuthenticated={setAuthenticated}/>
+                    </Protected>
+                } />
+
+                <Route path="/authenticate" element={<Authenticate setAuthenticated={setAuthenticated}/>} />
             </Routes>
         </BrowserRouter>
     )

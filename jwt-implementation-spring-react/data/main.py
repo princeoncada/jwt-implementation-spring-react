f627797123ea1fc7cc328c8d885fab5cf097cd19
Prince Oncada <prince.oncada@gmail.com>
2023-10-18 04:49:34 +0800

    data backend main -- refactored the fetch_stock_data endpoint by putting requests into a dictionary and checking if the dictionary contains the same request already. if it contains, current running request will be cancelled.

--- jwt-implementation-spring-react/data/main.py
+++ jwt-implementation-spring-react/data/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+import asyncio
+
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from data_retrieval.stock_scraper import scrape
 from data_processing.stock_parser import parser
@@ -25,23 +27,29 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+running_tasks = {}
 
 
 @app.get("/stock/{ticker}")
 async def fetch_stock_data(ticker: str):
-    result_data = {}
-    try:
-        stock_data = scrape(ticker)
-        result_data = parser(stock_data, ticker)
-    except Exception as e:
-        result_data = {"error": e}
-    finally:
-        return result_data
+    if ticker in running_tasks:
+        running_tasks[ticker].cancel()
+        del running_tasks[ticker]
 
+    try:
+        loop = asyncio.get_event_loop()
+        task = loop.run_in_executor(None, lambda: scrape(ticker))
+        running_tasks[ticker] = task
+        result = await task
+    except asyncio.CancelledError:
+        raise HTTPException(status_code=400, detail="Request canceled due to a new request with the same ticker.")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        if ticker in running_tasks:
+            del running_tasks[ticker]
+
+    return parser(result, ticker)
 
 if __name__ == '__main__':
     uvicorn.run(app, port=2000, host='localhost')

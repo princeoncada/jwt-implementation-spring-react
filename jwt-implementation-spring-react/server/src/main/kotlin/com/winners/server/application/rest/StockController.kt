c3c42bea15a4c4084df2ed2e30073f8882611455
Prince Oncada <prince.oncada@gmail.com>
2023-10-18 04:47:28 +0800

    controller -- added getTopStocks endpoint

--- jwt-implementation-spring-react/server/src/main/kotlin/com/winners/server/application/rest/StockController.kt
+++ jwt-implementation-spring-react/server/src/main/kotlin/com/winners/server/application/rest/StockController.kt
@@ -1,5 +1,6 @@
 package com.winners.server.application.rest
 
+import com.winners.server.application.dto.StockDTOs
 import com.winners.server.domain.model.Stock
 import com.winners.server.domain.service.StockService
 import org.springframework.http.ResponseEntity
@@ -20,6 +21,16 @@ class StockController(
         return try {
             val stock = stockService.getStockByTicker(ticker)
             ResponseEntity.ok(stock)
+        } catch (e: Exception) {
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    @GetMapping("/top")
+    fun getTopStocks(): ResponseEntity<List<StockDTOs.GetResult>> {
+        return try {
+            val stocks = stockService.getTopStocks()
+            ResponseEntity.ok(stocks)
         } catch (e: Exception) {
             ResponseEntity.notFound().build()
         }

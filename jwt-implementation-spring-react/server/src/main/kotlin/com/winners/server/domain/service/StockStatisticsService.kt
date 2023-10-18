71f502436a22d156448a46e0dd9dc06573a93663
Prince Oncada <prince.oncada@gmail.com>
2023-10-18 13:40:25 +0800

    back-end -- implemented getting all the stocks that meet entered stock filter parameters

---
+++ jwt-implementation-spring-react/server/src/main/kotlin/com/winners/server/domain/service/StockStatisticsService.kt
@@ -0,0 +1,20 @@
+package com.winners.server.domain.service
+
+import com.winners.server.domain.model.StockStatistics
+import jakarta.persistence.criteria.CriteriaBuilder
+import jakarta.persistence.criteria.Predicate
+import jakarta.persistence.criteria.Root
+import org.springframework.data.jpa.domain.Specification
+import org.springframework.stereotype.Service
+
+@Service
+interface StockStatisticsService {
+    fun buildPredicate(
+        root: Root<StockStatistics>,
+        criteriaBuilder: CriteriaBuilder,
+        filterName: String,
+        filterValues: List<Double>
+    ): Predicate
+    fun buildSpecifications(filters: Map<String, List<Double>>): List<Specification<StockStatistics>>
+    fun findStocksWithDynamicFilters(filters: Map<String, List<Double>>): List<StockStatistics>
+}

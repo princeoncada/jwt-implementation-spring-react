1b82f27567c4186f40d3878845e94600f74e51b6
Prince Oncada <prince.oncada@gmail.com>
2023-10-18 13:41:09 +0800

    back-end -- getMinMaxValues working -- still working on finding stocks by filter params

--- jwt-implementation-spring-react/server/src/main/kotlin/com/winners/server/application/rest/StockController.kt
+++ jwt-implementation-spring-react/server/src/main/kotlin/com/winners/server/application/rest/StockController.kt
@@ -3,16 +3,20 @@ package com.winners.server.application.rest
 import com.winners.server.application.dto.StockDTOs
 import com.winners.server.domain.model.Stock
 import com.winners.server.domain.service.StockService
+import com.winners.server.domain.service.StockStatisticsService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/stock")
 class StockController(
-    private val stockService: StockService
+    private val stockService: StockService,
+    private val stockStatisticsService: StockStatisticsService
 ) {
     @GetMapping("/{ticker}")
     fun getStock(
@@ -33,6 +37,26 @@ class StockController(
             ResponseEntity.ok(stocks)
         } catch (e: Exception) {
             ResponseEntity.notFound().build()
+        }
+    }
+
+    @GetMapping("/minmax")
+    fun getMinMaxValues(): ResponseEntity<MutableMap<String, DoubleArray>> {
+        return try {
+            val minMaxValues = stockService.getMinMaxValues()
+            ResponseEntity.ok(minMaxValues)
+        } catch (e: Exception) {
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    @PostMapping("/test")
+    fun test(@RequestBody body: Map<String, List<Double>>) {
+        return try {
+            val stocks = stockStatisticsService.findStocksWithDynamicFilters(body)
+            println(stocks)
+        } catch (e: Exception) {
+            println(e)
         }
     }
 }

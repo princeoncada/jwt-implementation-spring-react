edf12fc02686f1c802c4dc92a1a554ec1da318d2
Prince Oncada <prince.oncada@gmail.com>
2023-10-20 01:31:24 +0800

    backend -- refactored getStocksByUser return type

--- jwt-implementation-spring-react/server/src/main/kotlin/com/winners/server/domain/service/UserStockService.kt
+++ jwt-implementation-spring-react/server/src/main/kotlin/com/winners/server/domain/service/UserStockService.kt
@@ -1,12 +1,13 @@
 package com.winners.server.domain.service
 
+import com.winners.server.application.dto.StockDTOs
 import com.winners.server.application.dto.UserStockDTOs
 import com.winners.server.domain.model.UserStock
 import org.springframework.stereotype.Service
 
 @Service
 interface UserStockService {
-    fun getStocksByUser(email: String): List<UserStock>
+    fun getStocksByUser(email: String): List<StockDTOs.GetResult>
     fun addStockToUser(email: String, entityRequest: UserStockDTOs.PostRequest): UserStockDTOs.GetResult
     fun deleteStockFromUser(email: String, ticker: String): List<UserStockDTOs.GetResult>
     fun hasStock(email: String, ticker: String): Boolean

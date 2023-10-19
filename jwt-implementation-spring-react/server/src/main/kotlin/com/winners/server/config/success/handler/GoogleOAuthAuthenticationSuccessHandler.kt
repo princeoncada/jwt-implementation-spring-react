da8a7ef1fac68adedbf29c30a83c75b6fa7725ba
Prince Oncada <prince.oncada@gmail.com>
2023-10-20 05:12:55 +0800

    backend -- chore: removed unused import

--- jwt-implementation-spring-react/server/src/main/kotlin/com/winners/server/config/success/handler/GoogleOAuthAuthenticationSuccessHandler.kt
+++ jwt-implementation-spring-react/server/src/main/kotlin/com/winners/server/config/success/handler/GoogleOAuthAuthenticationSuccessHandler.kt
@@ -9,7 +9,6 @@ import jakarta.servlet.http.Cookie
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.core.Authentication
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 import java.time.Instant
